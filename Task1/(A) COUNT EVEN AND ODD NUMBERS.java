3102a83f30d1a0351251eab1c55d5075c29bc19c
VEMIREDDY VAMSI KRISHNA REDDY <vamsikrishnareddyvemireddy2006@gmail.com>
2026-02-16 18:47:03 +0530

    commit

---
+++ Task1/(A) COUNT EVEN AND ODD NUMBERS.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class EvenOddCount {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int even = 0;
+        int odd = 0;
+
+        
+        System.out.println("Enter 5 numbers:");
+        for (int i = 0; i < 5; i++) {
+            int num = sc.nextInt();
+            
+            
+            if (num % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+
+        
+        System.out.println("--- Results ---");
+        System.out.println("Even numbers = " + even);
+        System.out.println("Odd numbers = " + odd);
+        
+        sc.close(); 
+}
